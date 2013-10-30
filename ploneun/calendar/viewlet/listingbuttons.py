d56d5d5f023b8fca14b278c3033ade079a55de50
Izhar Firdaus <izhar@inigo-tech.com>
2013-10-30 22:47:36 +0800

    add listingbuttons viewlet (re #887)

---
+++ ploneun/calendar/viewlet/listingbuttons.py
@@ -0,0 +1,50 @@
+from Acquisition import aq_inner
+from zope.interface import Interface
+from five import grok
+from zope.component import getMultiAdapter
+from Products.CMFCore.interfaces import IContentish
+from plone.app.layout.viewlets import interfaces as manager
+from ploneun.calendar.interfaces import IProductSpecific
+from Solgema.fullcalendar.interfaces import ISolgemaFullcalendarMarker
+import os
+
+grok.templatedir('templates')
+
+
+class ListingButtons(grok.Viewlet):
+    grok.context(ISolgemaFullcalendarMarker)
+    grok.viewletmanager(manager.IBelowContentTitle)
+    grok.template('listingbuttons')
+    grok.layer(IProductSpecific)
+
+    def available(self):
+        return True
+
+    def buttons(self):
+        current_view = os.path.basename(self.request.getURL())
+        url = self.context.absolute_url()
+        buttons = [{
+             'id': 'solgemafullcalendar_view',
+             'url': url + '/solgemafullcalendar_view',
+             'label': u'Calendar',
+            }, {
+             'id': 'list_all',
+             'url': url + '/list_all',
+             'label': u'List View',
+            }, {
+             'id': 'list_upcoming',
+             'url': url + '/list_upcoming',
+             'label': u'Upcoming',
+            }, {
+             'id': 'list_past',
+             'url': url + '/list_past',
+             'label': u'Past'
+        }]
+
+        for button in buttons:
+            if button['id'] == current_view:
+                button['checked'] = True
+            else:
+                button['checked'] = False
+
+        return buttons
